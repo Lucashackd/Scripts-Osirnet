06cb83fe5745499629ba26b11161adb522e1f67d
Lucas Hackbart Döhnert <33258938+Lucashackd@users.noreply.github.com>
2026-09-11 15:36:24 -0300

    Fix download and update URLs in user script
    Updated download and update URLs for the script.

--- Osir - Destaque de troca de endereço.user.js
+++ Osir - Destaque de troca de endereço.user.js
@@ -8,8 +8,8 @@
 // @grant        none
 // @license      MIT
 // @homepageURL  https://github.com/Lucashackd/Scripts-Osirnet
-// @downloadURL  https://raw.githubusercontent.com/Lucashackd/Scripts-Osirnet/main/
-// @updateURL    https://raw.githubusercontent.com/Lucashackd/Scripts-Osirnet/main/
+// @downloadURL  https://raw.githubusercontent.com/Lucashackd/Scripts-Osirnet/main/Osir%20-%20Destaque%20de%20troca%20de%20endere%C3%A7o.user.js
+// @updateURL    https://raw.githubusercontent.com/Lucashackd/Scripts-Osirnet/main/Osir%20-%20Destaque%20de%20troca%20de%20endere%C3%A7o.user.js
 // @supportURL   https://github.com/Lucashackd/Scripts-Osirnet/issues
 // ==/UserScript==
 
